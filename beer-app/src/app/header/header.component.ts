4bdfc097bc520fe3cbb4ef03bd8bb1050aaaed41
Matt Moore <matt.e.moore@gmail.com>
2019-12-16 18:43:17 -0600

    form and form reset logic

--- beer-app/src/app/header/header.component.ts
+++ beer-app/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
 import { SearchService } from '../search.service';
 
 @Component({
@@ -8,18 +11,25 @@ import { SearchService } from '../search.service';
 })
 
 export class HeaderComponent implements OnInit {
-
-  searchTerm: string = "Hops";
-  constructor(private service: SearchService) { }
+  searchForm: FormGroup;
+  constructor(private service: SearchService, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      searchTerm: ['', Validators.required]
+    })
   }
 
-
   search() {
-    this.service.search(this.searchTerm)
+    if (this.searchForm.invalid || !this.searchForm.dirty) {
+      return;
+    }
+    let field = this.searchForm.get("searchTerm");
+    let searchString = field.value;
+    this.service.search(searchString);
+    this.searchForm.reset({searchTerm: searchString});
     // TODO:
-    // Use some type of Observable to send search result updates to list component
+    // Use some type of Observable to send search result updates to beer-list-component
     // Do we do that here or in the service?
   }
 }
